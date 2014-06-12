e9913298fd1c48d67769b392627c61550ccad076
dmitryu <reedwalter24@gmail.com>
2014-06-12 15:51:00 +0300

    Exersices

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,6 +21,21 @@ describe "Static pages" do
 
   describe "Сontact page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title('Home'))
   end
 end
